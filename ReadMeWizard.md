41704ac85918129aaa22f45942c080810e1ec4c5
Ethan Chandler <86173119+IzzTinkr@users.noreply.github.com>
2021-10-04 18:52:07 -0500

    added markdown styling and passed through writeFile, started on TOC as well

--- ReadMeWizard.md
+++ ReadMeWizard.md
@@ -1,1 +1,46 @@
-hello world in read me
+
+  
+**Test Title**
+---
+
+
+*Table of Contents*
+---
+<a href="https://github.com/izztnkr">Description</a>
+
+<a href="https://github.com/izztnkr">Installation</a>
+
+<a href="https://github.com/izztnkr">Usage</a>
+
+<a href="https://github.com/izztnkr">License</a>
+
+<a href="https://github.com/izztnkr">Contributing</a>
+
+<a href="https://github.com/izztnkr">Tests</a>
+
+<a href="https://github.com/izztnkr">Questions</a>
+
+---
+**Description**
+---
+---
+**Installation**
+---
+---
+**Usage**
+---
+---
+**License**
+---
+---
+**Contributing**
+---
+
+---
+**Tests**
+---
+---
+**Questions**
+---
+  
+  
--- index2.js
+++ index2.js
@@ -9,35 +9,35 @@ const Choices = require("inquirer/lib/objects/choices");
 //tell computer to use inquirer to ask a set of questions and recieve answers
 //tell computer to run it
 //from command line "node index2.js"
-const questions = [
-  {
-    type: "input",
-    name: "first_name",
-    message: "What's your first name?",
-  },
-  {
-    type: "input",
-    name: "last_name",
-    message: "What's your last name?",
-  },
-];
+// const questions = [
+//   {
+//     type: "input",
+//     name: "first_name",
+//     message: "What's your first name?",
+//   },
+//   {
+//     type: "input",
+//     name: "last_name",
+//     message: "What's your last name?",
+//   },
+// ];
 
-inquirer
-  .prompt(questions)
-  .then((answers) => {
-    // answers = (first_name, +"" + last_name);
-    // console.log(answers);
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      console.log(
-        "Couldn't be rendered in the current environment. Yes, this is an error message"
-      );
-    } else {
-      console.log("Something else went totes wrong");
-    }
-  });
+// inquirer
+//   .prompt(questions)
+//   .then((answers) => {
+//     // answers = (first_name, +"" + last_name);
+//     // console.log(answers);
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       console.log(
+//         "Couldn't be rendered in the current environment. Yes, this is an error message"
+//       );
+//     } else {
+//       console.log("Something else went totes wrong");
+//     }
+//   });
 
 //-----STRING or TEMPLATE LITERAL-----//
 //tell computer to display answers
@@ -50,11 +50,68 @@ inquirer
 // //place this in the .then part of the inquirer prompt
 
 //-----FILE or WRITE FILE-----//
+//file styling//
+const name = "izztnkr";
+const projecTitle = "Test Title";
+
+const generateMarkdownStyling = (name, projecTitle) => {
+  return `
+  
+**${projecTitle}**
+---
+
+
+*Table of Contents*
+---
+<a href="https://github.com/${name}">Description</a>
+
+<a href="https://github.com/${name}">Installation</a>
+
+<a href="https://github.com/${name}">Usage</a>
+
+<a href="https://github.com/${name}">License</a>
+
+<a href="https://github.com/${name}">Contributing</a>
+
+<a href="https://github.com/${name}">Tests</a>
+
+<a href="https://github.com/${name}">Questions</a>
+
+---
+**Description**
+---
+---
+**Installation**
+---
+---
+**Usage**
+---
+---
+**License**
+---
+---
+**Contributing**
+---
+
+---
+**Tests**
+---
+---
+**Questions**
+---
+  
+  `;
+};
+
 //write a file
 //what kind of file? readme.md essentially an .md file or markdown file
 //how do we do that? we have to write a promise within the "generateMarkdown.js" file (see 9.2.5 & 9.5.4 in module readings)
-fs.writeFile("ReadMeWizard.md", "hello world in read me", (err) => {
-  //replace "hello world in read me" with the template literal //
-  if (err) throw err;
-  console.log("doneskies! You can check for file now.");
-});
+fs.writeFile(
+  "./ReadMeWizard.md",
+  generateMarkdownStyling(name, projecTitle),
+  (err) => {
+    //replace "hello world in read me" with the template literal //
+    if (err) throw err;
+    console.log("doneskies! You can check for file now.");
+  }
+);
